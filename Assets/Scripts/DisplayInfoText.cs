4d2e2563b4c1c1134771654bd45bf825946d17e4
Brett Davis <brett.p.davis101@gmail.com>
2021-04-22 18:54:42 -0500

    Through task 6

---
+++ Assets/Scripts/DisplayInfoText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayInfoText : MonoBehaviour
+{
+    public GameObject InfoBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void displayInfoBox() {
+        InfoBox.SetActive(!InfoBox.gameObject.activeSelf);
+    }
+}
